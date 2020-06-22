3b1feb7a9c6d071310781538cb61b0cfe8139bd9
Neelikanth23 <64316630+Neelikanth23@users.noreply.github.com>
2020-06-22 22:33:25 +0530

    Update Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => (
   <div className="footer">
-    <p>Designed and developed by: Hamza - <a rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/hmz.js/?hl=en">@hmz.js</a></p>
+    <p>Designed and developed by: Neelikanth - <a rel="noopener noreferrer" target="_blank" href="https://www.instagram.com">@Neel</a></p>
   </div>
 )
 
